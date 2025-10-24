8a2ecd895396714fe4950dcb5b445e0beff48501
anubhav710 <anubhavagrawal710@gmail.com>
2025-10-24 11:09:31 +0530

    added favicon

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -25,8 +25,8 @@ export default function Home() {
           scale={isMobile ? 0.8 : 1}
         />
       </View>
-      <p className="absolute z-10 top-11/12 place-self-center md:text-xs text-[10px] font-medium tracking-wider text-white/80 uppercase">
-        Select a product to begin
+      <p className="absolute z-10 top-11/12 place-self-center text-white/80 md:text-xs text-[10px] font-medium tracking-wider">
+        SELECT A PRODUCT TO BEGIN
       </p>
       <div
         className="absolute z-20 top-10/12 left-1/12 bg-white mask-[url(/icons/left.svg)] size-12 mask-no-repeat border hover-animation md:hidden block"
